f4c097d39847fa4648b0777eb1f2087746bd29b5
souvikMitraHda <42381596+souvikMitraHda@users.noreply.github.com>
2022-05-12 13:09:52 +0200

    Update ForeignExchangeApplication.java

--- src/main/java/com/fundsaccess/ForeignExchange/ForeignExchangeApplication.java
+++ src/main/java/com/fundsaccess/ForeignExchange/ForeignExchangeApplication.java
@@ -2,12 +2,24 @@ package com.fundsaccess.ForeignExchange;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
-public class ForeignExchangeApplication {
+public class ForeignExchangeApplication implements CommandLineRunner {
 
+	@Value("${gar.location}")
+	private String garLocation;
+	
 	public static void main(String[] args) {
+		system.out
 		SpringApplication.run(ForeignExchangeApplication.class, args);
 	}
+	
+	@Override
+	public void run(String args[]) throws Exception
+	{
+	   System.out.println("Username from environment variable : " + garLocation);
+	}
+	
 
 }
