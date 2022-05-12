8dc712179a076505cb7b86c799baea7d3f7c6a55
souvikMitraHda <42381596+souvikMitraHda@users.noreply.github.com>
2022-05-12 13:11:54 +0200

    Update ForeignExchangeApplication.java

--- src/main/java/com/fundsaccess/ForeignExchange/ForeignExchangeApplication.java
+++ src/main/java/com/fundsaccess/ForeignExchange/ForeignExchangeApplication.java
@@ -3,6 +3,7 @@ package com.fundsaccess.ForeignExchange;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.beans.factory.annotation.Value;
 
 @SpringBootApplication
 public class ForeignExchangeApplication implements CommandLineRunner {
